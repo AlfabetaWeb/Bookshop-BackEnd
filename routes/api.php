17a53190913d74aa42a750ed39b0d6c1c6115f53
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-09-04 00:20:59 +0200

    feat: Create login route and fix DB connection

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use App\Http\Controllers\AuthController;
 
 //GENERAL ACCESS USER
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
