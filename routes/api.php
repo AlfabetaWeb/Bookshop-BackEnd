db1669dfe5011db7fc93e582e27af0ae7c864073
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-09-04 19:07:50 +0200

    feat: Link routes por user profile and logout

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,8 @@ use App\Http\Controllers\AuthController;
 //GENERAL ACCESS USER
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(["middleware" => "jwt.auth"] , function() {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']); 
+});
