a6ff180cddfb87027ca8b377e963f011caaec401
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-10-02 22:47:24 +0200

    feat: Edit User Class

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable implements JWTSubject
         {
             return [];
         }
+       
+    public function clubs()
+        {
+            return $this->belongsToMany(Club::class);
+        }
 }
