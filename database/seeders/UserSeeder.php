1bf6cb6b17c83f7ec5aab6036b359df3a9532096
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-10-10 05:19:00 +0200

    feat: Fix seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -15,9 +17,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('user')->insert([         
-            'name'=> 'lectorOne',
-            'email' => 'one@mail.com',
-            'password'=> 'marcador',
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
         ]);
     }
 }
